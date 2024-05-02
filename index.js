2261f24cf53c08302ff60cbbbd651c7501be1ec5
DanielPscolovki <151840169+DanielPscolovki@users.noreply.github.com>
2024-05-02 17:56:09 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let numberOne = 10;
+let numberTwo = 20;
+console.log(numberOne + numberTwo); // 30
+console.log(numberOne - numberTwo); // -10
+console.log(numberOne * numberTwo); // 200
+console.log(numberOne / numberTwo); // 0.5
+console.log(numberOne % numberTwo); // 10
+console.log(numberOne ** numberTwo); // 100000000000000000000
+console.log(numberOne++); // 10
+console.log(++numberOne); // 11
+console.log(numberOne--); // 11
+console.log(--numberOne); // 10
+console.log(--numberOne + numberTwo++); // 29
+console.log(numberOne-- - --numberTwo + numberTwo++); // -10
+console.log(numberOne++ / numberTwo++); // 0.45454545454545453
+console.log(numberOne++ * --numberTwo); // 90
